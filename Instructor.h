a045441a05080514e3640887450d0314145c205d
Ammar-Quorah <merothehero111@gmail.com>
2025-11-11 09:48:11 +0200

    Update Instructor.h
    completed Instructor class

--- Instructor.h
+++ Instructor.h
@@ -2,18 +2,47 @@
 #define INSTRUCTOR_H
 
 #include "Person.h"
-using namespace std;
-//#write Instructor class here
 
+class Instructor : public Person {
+private:
+    string department;
+    int experienceYears;
 
+public:
+    Instructor() : Person() {
+        department = "";
+        experienceYears = 0;
+    }
 
+    Instructor(string n, int i, string d, int e) : Person(n, i) {
+        department = d;
+        experienceYears = e;
+    }
 
+    ~Instructor() {}
 
+    void setDepartment(string d) {
+        department = d;
+    }
 
+    void setExperienceYears(int e) {
+        experienceYears = e;
+    }
 
+    string getDepartment() {
+        return department;
+    }
 
+    int getExperienceYears() {
+        return experienceYears;
+    }
 
-
-
+    void display() {
+        cout << "\nInstructor Info:" << endl;
+        Person::display();
+        cout << "Department: " << department << endl;
+        cout << "Experience: " << experienceYears << " years" << endl;
+    }
+};
 
 #endif
