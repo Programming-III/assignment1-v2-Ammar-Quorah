0b953560e35a130a22330e8de6c41ee5556a3369
Ammar-Quorah <merothehero111@gmail.com>
2025-11-11 09:49:41 +0200

    Update Student.h
    completed Student class

--- Student.h
+++ Student.h
@@ -2,20 +2,47 @@
 #define STUDENT_H
 
 #include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
 
+class Student : public Person {
+private:
+    int yearLevel;
+    string major;
 
+public:
+    Student() : Person() {
+        yearLevel = 0;
+        major = "";
+    }
 
+    Student(string n, int i, int y, string m) : Person(n, i) {
+        yearLevel = y;
+        major = m;
+    }
 
+    ~Student() {}
 
+    void setYearLevel(int y) {
+        yearLevel = y;
+    }
 
+    void setMajor(string m) {
+        major = m;
+    }
 
+    int getYearLevel() {
+        return yearLevel;
+    }
 
+    string getMajor() {
+        return major;
+    }
 
-
-
-
+    void display() {
+        cout << "\nStudent Info:" << endl;
+        Person::display();
+        cout << "Year: " << yearLevel << endl;
+        cout << "Major: " << major << endl;
+    }
+};
 
 #endif
