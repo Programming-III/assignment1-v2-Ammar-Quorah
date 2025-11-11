0fbc123a8ea98616189f81d527b0d16f78df3d8a
Ammar-Quorah <merothehero111@gmail.com>
2025-11-11 09:46:49 +0200

    Update Course.h
    completed course class

--- Course.h
+++ Course.h
@@ -1,22 +1,59 @@
 #ifndef COURSE_H
 #define COURSE_H
 
-#include <string>
+#include "Instructor.h"
 #include "Student.h"
-using namespace std;
-//#write your code here
 
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+    Instructor instructor;
 
+public:
+    Course() {
+        courseCode = "";
+        courseName = "";
+        maxStudents = 0;
+        students = nullptr;
+        currentStudents = 0;
+    }
 
+    Course(string code, string name, int max, Instructor instr) {
+        courseCode = code;
+        courseName = name;
+        maxStudents = max;
+        instructor = instr;
+        currentStudents = 0;
+        students = new Student[maxStudents];
+    }
 
+    ~Course() {
+        delete[] students;
+    }
 
+    void addStudent(Student s) {
+        if (currentStudents < maxStudents) {
+            students[currentStudents] = s;
+            currentStudents++;
+        } else {
+            cout << "Course is full" << endl;
+        }
+    }
 
+    void displayCourseInfo() {
+        cout << "\nCourse: " << courseCode << " - " << courseName << endl;
+        cout << "Max Students: " << maxStudents << endl;
+        cout << "Currently Enrolled: " << currentStudents << endl;
+        instructor.display();
 
-
-
-
-
-
-
+        for (int i = 0; i < currentStudents; i++) {
+            students[i].display();
+        }
+}
+};
 
 #endif
