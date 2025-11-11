eb92e156490bfdc950a8c7dca0ff1f11adbb8a16
Ammar-Quorah <merothehero111@gmail.com>
2025-11-11 09:48:55 +0200

    Update Person.h
    completed Person class

--- Person.h
+++ Person.h
@@ -1,20 +1,48 @@
 #ifndef PERSON_H
 #define PERSON_H
 
+#include <iostream>
 #include <string>
 using namespace std;
 
-//#write Person class here
+class Person {
+private:
+    string name;
+    int id;
 
+public:
+    Person() {
+        name = "";
+        id = 0;
+    }
 
+    Person(string n, int i) {
+        name = n;
+        id = i;
+    }
 
+    ~Person() {}
 
+    void setName(string n) {
+        name = n;
+    }
 
+    void setId(int i) {
+        id = i;
+    }
 
+    string getName() {
+        return name;
+    }
 
+    int getId() {
+        return id;
+    }
 
-
-
-
+    virtual void display() {
+        cout << "Name: " << name << endl;
+        cout << "ID: " << id << endl;
+    }
+};
 
 #endif
